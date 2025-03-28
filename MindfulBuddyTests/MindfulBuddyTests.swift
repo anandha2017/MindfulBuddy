69605d8f0eb23b0fe8c8f22ab99139b6b801f795
anandha-at-486636921885 <github2017@ponnampalam.ca>
2025-03-28 23:17:24 +0000

    Initial Commit

---
+++ MindfulBuddyTests/MindfulBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  MindfulBuddyTests.swift
+//  MindfulBuddyTests
+//
+//  Created by Anandha Ponnampalam on 28/03/2025.
+//
+
+import Testing
+@testable import MindfulBuddy
+
+struct MindfulBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
